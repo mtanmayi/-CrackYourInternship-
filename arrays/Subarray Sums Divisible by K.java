8016d1be11815ad7b52ddd2694f52410372ebabd
mtanmayi <68344249+mtanmayi@users.noreply.github.com>
2022-07-16 15:14:04 +0530

    Create Subarray Sums Divisible by K.java

---
+++ arrays/Subarray Sums Divisible by K.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int subarraysDivByK(int[] nums, int k) {
+        
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(0,1);
+        int count = 0, sum = 0;
+        
+        for(int i=0;i<nums.length;i++){  
+            sum+= nums[i];
+            
+            int remainder = sum%k;
+            if(remainder < 0) remainder += k;
+            
+            if(map.containsKey(remainder)){
+                count+= map.get(remainder);
+                map.put(remainder, map.get(remainder)+1);
+            }else{
+                map.put(remainder,1);
+            }  
+        }
+        return count; 
+    }
+}
