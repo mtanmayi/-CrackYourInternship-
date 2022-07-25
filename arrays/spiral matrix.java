8108ad62190b64af9cdfc4610b8d3ab7ea232bdf
mtanmayi <68344249+mtanmayi@users.noreply.github.com>
2022-07-26 02:45:51 +0530

    Update spiral matrix.java

--- arrays/spiral matrix.java
+++ arrays/spiral matrix.java
@@ -1,34 +1,40 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> li = new ArrayList<Integer>();
-        int maxr = matrix.length-1;
-        int maxc = matrix[0].length-1;
-        int i,j;
-        int minr = 0, minc = 0, t = matrix.length*matrix[0].length, c = 0;
-        while(c<t)
-        {
-            for(i = minr, j = minc; j<=maxc && c<t; j++,c++)
-            {
-                li.add(matrix[i][j]);
-            }
-            minr++;
-            for(i = minr, j = maxc; i<=maxr && c<t; i++,c++)
-            {
-                li.add(matrix[i][j]);
-            }
-            maxc--;
-            for(i = maxr, j = maxc; j>=minc && c<t; j--,c++)
-            {
-                li.add(matrix[i][j]);
-            }
-            maxr--;
-            for(i = maxr, j = minc; i>=minr && c<t; i--,c++)
-            {
-                li.add(matrix[i][j]);
-            }
-            minc++;
-        }
-        return li;
+      List<Integer> list = new ArrayList<>(); int direction=0;
+      
+        int row=matrix.length;
+        int col=matrix[0].length;
         
-    }
+        int top = 0, bottom = row-1;
+        int left = 0,right = col-1;
+        
+        while(top <= bottom && right>=left ){
+            if(direction==0){
+              for(int i=left;i<=right;i++)
+                list.add(matrix[top][i]);
+              top++;
+              direction=1;
+            }
+            else if(direction==1){
+               for(int i = top; i <= bottom; i++)
+                  list.add(matrix[i][right]);
+                right--;
+                direction = 2;
+            }
+            else if(direction==2){
+                for(int i=right;i>=left;i--)
+                 list.add(matrix[bottom][i]);
+              bottom--;
+              direction=3;
+            }
+            else if(direction==3){
+                for(int i=bottom;i>=top;i--)
+                 list.add(matrix[i][left]);
+               left++;
+               direction =0;
+            }
+        }
+        
+        return list;
+  }
 }
