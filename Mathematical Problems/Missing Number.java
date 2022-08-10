e8671029129e0be1323089ac1ba052eefc1ca4d6
mtanmayi <68344249+mtanmayi@users.noreply.github.com>
2022-08-11 01:37:37 +0530

    Update Missing Number.java

--- Mathematical Problems/Missing Number.java
+++ Mathematical Problems/Missing Number.java
@@ -12,3 +12,16 @@ class Solution {
         return x;
     }
 }
+-----------------------------------------------------
+    class Solution {
+    public int missingNumber(int[] nums) {
+        int n=nums.length;
+        int sum=0;
+        int tsum=0;
+        for(int i=0;i<n;i++)
+            tsum+=nums[i];
+        sum=(n*(n+1))/2;
+        int ans=sum-tsum;
+        return ans;
+    }
+}
